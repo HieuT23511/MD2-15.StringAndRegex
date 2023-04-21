4aa999ae74a58577d2c2bf82095566998cf1429f
HieuT23511 <hieut23511@gmail.com>
2023-04-21 10:20:44 +0700

    Validate Email

---
+++ 1.ValidateEmail.ts
@@ -0,0 +1,19 @@
+export class EmailRegex {
+    private EMAIL_REGEX: RegExp = /^[A-Za-z0-9]+[A-Za-z0-9]*@[A-Za-z0-9]+(\.[A-Za-z0-9]+)$/;
+    public validate(regex: string): boolean {
+        return this.EMAIL_REGEX.test(regex);
+    }
+}
+let emailRegex = new EmailRegex();
+let validEmail: string[] = ["a@gmail.com", "ab@yahoo.com", "abc@hotmail.com"];
+let invalidEmail: string[] = ["@gmail.com", "ab@gmail.", "@#abc@gmail.com"];
+
+validEmail.forEach(elements => {
+    let isValid: boolean = emailRegex.validate(elements);
+    console.log(`Email is ${elements} is valid: ${isValid}`)
+})
+
+for (let email of invalidEmail) {
+    let isValid: boolean = emailRegex.validate(email);
+    console.log("Email is " + email + " is valid: " + isValid);
+}
